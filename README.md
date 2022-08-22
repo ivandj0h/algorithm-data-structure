4de770aa3f21f4dc213638c2d57920afe87d4c52
ivandjoh <ivandi.djoh@gmail.com>
2022-08-21 17:54:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## algorithm-data-structure
+
+### About 
+ Algorithms and data structures implemented in JavaScript
---
+++ index.js
@@ -0,0 +1,8 @@
+const listsData = [
+    {
+        title: 'data-structure',
+        cmd: 'node data-structure/index.js'
+    }
+]
+
+console.log(listsData[0].title)
